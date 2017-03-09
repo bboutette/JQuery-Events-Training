76419ac1cf3a360b78e2cbd9e5c80d9a637e3174
b b <bboutette@gmail.com>
2017-03-08 23:52:13 -0600

    Tried to get sum value of first two boxes, but couldn't get it to work.

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,23 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-  $('body').html('<h1>Go!</h2>');
+  $('body').append('<h2>Go!</h2>');
   $("#time").text( Date.now() );
 
+  $(function() {
+  $('#right').keydown function(evt){
+        var numVal1 = $('#left').val();
+        var numVal2 = $('#right').val();
+        var result = parseInt(numVal1) + parseInt(numVal2);
+        if(!isNaN(result) &&
+        if(evt.keyCode == 9) {
+           $('#total').val() = result;
+        })
+    })
 })
+  // $('#btn-show').keyup(function() {
+  //     var firstName = $('#fname').val();
+  //     var lastName = $('#lname').val();
+  //
+  //     $('#first').text(firstName);
+  //     $('#last').text(lastName);
